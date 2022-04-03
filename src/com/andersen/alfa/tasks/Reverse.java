562186c43f5b05572fdd2bef9b7e1179c547daec
Vitali Dudarau <v.dudarau@andersenlab.com>
2022-04-03 16:18:58 +0300

    Add task which names "Reverse"

---
+++ src/com/andersen/alfa/tasks/Reverse.java
@@ -0,0 +1,27 @@
+// Reverse the line
+package com.andersen.alfa.tasks;
+
+import java.util.Scanner;
+
+public class Reverse {
+
+    @SuppressWarnings("resources")
+    public static String reverse(String line1) {
+        char[] array = line1.toCharArray();
+        String line2 = "";
+        for (int i = array.length-1; i >= 0; i--) {
+            line2 += array[i];
+        }
+        return line2;
+    }
+
+    public static void main(String[] args) {
+        String line1;
+
+        Scanner s =  new Scanner(System.in);
+        System.out.println("Write the line:");
+        line1 = s.nextLine();
+
+        System.out.println(reverse(line1));
+    }
+}

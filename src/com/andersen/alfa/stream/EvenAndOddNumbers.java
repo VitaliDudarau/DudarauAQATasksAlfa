36793f6508c351f387ecc96598f1e0e070bff3b3
Vitali Dudarau <v.dudarau@andersenlab.com>
2022-04-05 11:34:32 +0300

    Add EvenAndOddNumbers and changed PositiveCollection

--- src/com/andersen/alfa/stream/EvenAndOddNumbers.java
+++ src/com/andersen/alfa/stream/EvenAndOddNumbers.java
@@ -1,7 +1,9 @@
 /*
 Создать коллекцию целых чисел, написать методы:
-2.1. Который отрицательные числа делает положительными
-и возвращает коллекцию ([1, 5, -3, 7] -> [1, 5, 3, 7])
+2.2. Который четные числа умножает на 100, а от
+ нечетных отнимает 100 и возвращает коллекцию.
+ Количество принимаемых и возвращаемых элементов
+ коллекций должно совпадать
  */
 package com.andersen.alfa.stream;
 
@@ -9,14 +11,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class PositiveCollection {
+public class EvenAndOddNumbers {
 
     public static void main(String[] args) {
 
         List<Integer> array = Arrays.asList(25, 54, 56, -54, 5, -8);
 
         List<Integer> newArray = array.stream()
-                .map(Math::abs)
+                .map(x -> x % 2 == 0 ? x * 100: x - 100)
                 .collect(Collectors.toList());
 
         newArray.forEach(System.out::println);
